ac79e1400bf588edf978946ad242e23525caaa01
decanoe <deca.noe49@gmail.com>
2025-04-16 11:54:03 +0200

    adding SAT to visualization data and adding a temp file in case of early exit

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ void path_message() {
     exit(1);
 }
 
-void run_on_sat(const FileData& file_data) {
+void run_on_sat(const FileData& file_data, std::ofstream* output_file = nullptr) {
     std::cout << "reading file..." << std::endl;
     std::shared_ptr<Formule> f = std::shared_ptr<Formule>(new Formule(file_data.get_string("instance")));
     
@@ -37,25 +37,15 @@ void run_on_sat(const FileData& file_data) {
         temp = Argmax::tabu_search(solution.clone(), file_data.get_int("ban_list_size"), file_data.get_int("nb_iteration_max"));
     }
     else if (file_data.get_string("algorithm") == "evolution") {
-        temp = Argmax::evolution([solution]() -> std::unique_ptr<Instance> { return solution.randomize_clone(); }, Argmax::evolution_parameters(file_data));
+        temp = Argmax::evolution([solution]() -> std::unique_ptr<Instance> { return solution.randomize_clone(); }, Argmax::evolution_parameters(file_data), output_file);
     }
     Solution result = *dynamic_cast<Solution*>(temp.get());
 
     std::cout << "max score: " << result << " : " << int(result.score()) << " out of " << f->get_nb_clauses() << "\n";
 }
-void run_on_fa(const FileData& file_data) {
+void run_on_fa(const FileData& file_data, std::ofstream* output_file = nullptr) {
     std::cout << "reading files..." << std::endl;
     std::shared_ptr<Deck> deck = std::make_shared<Deck>(file_data.get_string("cards"), file_data.get_string("sanctuaries"));
-
-    std::string output_file_path = "./python/data/FA_" + file_data.get_string("algorithm") + "_" + timestamp() + ".rundata";
-    std::ofstream* output_file = nullptr;
-    if (file_data.get_bool("debug_screen", false)) {
-        output_file = new std::ofstream(output_file_path);
-        if (!(*output_file).is_open()) {
-            std::cerr << "\033[1;31mERROR: cannot open output file at \"" << output_file_path << "\" !\033[0m\n";
-            exit(1);
-        }
-    }
     
     Hand h(deck);
     h.randomize();
@@ -73,13 +63,6 @@ void run_on_fa(const FileData& file_data) {
 
     std::cout << "max score: " << int(result.score()) << " points with hand\n";
     result.pretty_cout(std::cout);
-
-    if ((*output_file).is_open()) {
-        (*output_file).close();
-        delete output_file;
-        std::cout << "data saved in " << output_file_path << std::endl;
-        system(("python ./python/data_visualizer.py " + output_file_path).c_str());
-    }
 }
 
 int main(int argc, char *args[]) {
@@ -91,8 +74,45 @@ int main(int argc, char *args[]) {
     
     FileData file_data = FileData(args[1]);
 
-    if (file_data.get_string("problem") == "SAT") run_on_sat(file_data);
-    if (file_data.get_string("problem") == "FA") run_on_fa(file_data);
+    std::ofstream* output_file = nullptr;
+    if (file_data.get_bool("debug_screen", false)) {
+        output_file = new std::ofstream("./temp.rundata");
+        if (!(*output_file).is_open()) {
+            std::cerr << "\033[1;31mERROR: cannot open output file at \"./temp.rundata\" !\033[0m\n";
+            exit(1);
+        }
+    }
+
+    if (file_data.get_string("problem") == "SAT") run_on_sat(file_data, output_file);
+    if (file_data.get_string("problem") == "FA") run_on_fa(file_data, output_file);
+    
+    if ((*output_file).is_open()) {
+        (*output_file).close();
+        delete output_file;
+
+        std::string output_file_path = "./python/data/FA_" + file_data.get_string("algorithm") + "_" + timestamp() + ".rundata";
+        std::ofstream final_file = std::ofstream(output_file_path);
+        if (!final_file.is_open()) {
+            std::cerr << "\033[1;31mERROR: cannot open output file at \"./temp.rundata\" !\033[0m\n";
+            exit(1);
+        }
+
+        std::ifstream temp_file = std::ifstream("./temp.rundata");
+        if (!temp_file.is_open()) {
+            std::cerr << "\033[1;31mERROR: cannot open output file at \"./temp.rundata\" !\033[0m\n";
+            exit(1);
+        }
+
+        std::string line; 
+        while (std::getline(temp_file, line)) final_file << line << "\n";
+
+        final_file.close();
+        temp_file.close();
+        std::remove("./temp.rundata");
+
+        std::cout << "data saved in " << output_file_path << std::endl;
+        system(("python ./python/data_visualizer.py " + output_file_path).c_str());
+    }
 
     return 0;
 }
--- presets/SAT_evo.md
+++ presets/SAT_evo.md
@@ -1,33 +1,18 @@
 # ====================================== problem to run and how to run it ======================================
-# FA / SAT
-str     problem = FA
-# hill_climb / tabu_search / evolution
+str     problem = SAT
 str     algorithm = evolution
-# whether or not to launch the Python viewer
 bool    debug_screen = true
-# if set, data will only be saved every <debug_generation_spacing> generation
-int     debug_generation_spacing = 64
+int     debug_generation_spacing = 16
 
 # ====================================== files containing data to run ======================================
-str     cards = ./FA/data/cards.txt
-str     sanctuaries = ./FA/data/sanctuary.txt
 str     instance = ./sat_specific/instances/BMS_k3_n100_m429/BMS_k3_n100_m429_0.cnf
 
-# ====================================== hill_climb parameters ======================================
-int     nb_iteration_max = 512
-
-# ====================================== tabu_search parameters ======================================
-int     nb_iteration_max = 2048
-int     ban_list_size = 25
-# not implemented
-int     ban_list_random_added_size = 5
-
 # ====================================== evolution parameters ======================================
-int     generation_count = 1024
+int     generation_count = 8096
 
 int     population_start_size = 256
-int     population_spawn_size = 64
-int     population_despawn_size = 64
+int     population_spawn_size = 1
+int     population_despawn_size = 1
 
 
 # nb of childs sawned per 2 parent
@@ -35,14 +20,14 @@ int     spawn_per_parent = 1
 # nb of individual from wich to pick each parent
 int     competition_goup_size = 2
 # probability of mutating each args
-float   mutation_probability = 0.1
+float   mutation_probability = 0.02
 # run a local search algorithm on childs when spawned
 # none / hill_climb / tabu_search / lambda_mutation
-str     run_algo_on_child = none
+str     run_algo_on_child = tabu_search
 # budget for running the local search algorithm on childs (1 mutation checked = 1 budget)
-int     child_algo_budget = 128
+int     child_algo_budget = 1000
 # lambda for running lambda_mutation or ban_list_length for running tabu_search
-int     child_algo_parameter = 8
+int     child_algo_parameter = 10
 # makes it impossible for newly spawned instances to be despawned
 bool    protect_child_from_despawn = true
 
@@ -54,7 +39,7 @@ float   despawn_criteria_diversity_multiplier = 0
 
 # islands not implemented yet
 int     nb_islands = 1
-int     generation_between_migrations = 32
+int     generation_between_migrations = -1
 float   percent_of_population_per_migrations = 0.05
 
 # show the best of each generation in debug while running the algorithm
